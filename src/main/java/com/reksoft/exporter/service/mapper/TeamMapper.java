2d527cd5db2a0c4771b0ffc1a43085567c0ab704
Nikita Baboshin <baboshinikki90@gmail.com>
2025-07-06 23:44:49 +0400

    update TeamMapper

--- src/main/java/com/reksoft/exporter/service/mapper/TeamMapper.java
+++ src/main/java/com/reksoft/exporter/service/mapper/TeamMapper.java
@@ -5,6 +5,7 @@ import com.reksoft.exporter.dto.PlayerDto;
 import com.reksoft.exporter.dto.TeamDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.util.Collections;
 import java.util.List;
@@ -13,9 +14,10 @@ import java.util.stream.Collectors;
 @Mapper(componentModel = "spring")
 public interface TeamMapper {
 
-    @Mapping(target = "playerNames", source = "players")
+    @Mapping(target = "playerNames", source = "players", qualifiedByName = "mapPlayersToNames")
     Team toTeam(TeamDto teamDto);
 
+    @Named("mapPlayersToNames")
     default List<String> mapPlayersToNames(List<PlayerDto> players) {
         if (players == null) return Collections.emptyList();
         return players.stream()
